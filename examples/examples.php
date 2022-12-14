12f67515126e5dc295f18d5c6b62eba4880ff997
Adrean Boyadzhiev <adrean.boyadzhiev@gmail.com>
2022-12-14 20:21:05 +0200

    Reformat the examples source code.

--- examples/examples.php
+++ examples/examples.php
@@ -11,14 +11,14 @@ $parser = new \Math\Parser();
 $expression = '1 + 2 * 3 * ( 7 * 8 ) - ( 45 - 10 )';
 $result = $parser->evaluate($expression);
 
-printf('%s => %f;%s', $expression, $result,  PHP_EOL);
+printf('%s => %f;%s', $expression, $result, PHP_EOL);
 
 $expression = '11 - -2 * -3 * ( 17 * 81 ) - ( -45 - 10 )';
 $result = $parser->evaluate($expression);
 
-printf('%s => %f;%s', $expression, $result,  PHP_EOL);
+printf('%s => %f;%s', $expression, $result, PHP_EOL);
 
 $expression = '-1 + -2 * 13 * ( 7 * 8 ) - ( 415 - 0.1 )';
 $result = $parser->evaluate($expression);
 
-printf('%s => %f;%s', $expression, $result,  PHP_EOL);
+printf('%s => %f;%s', $expression, $result, PHP_EOL);
